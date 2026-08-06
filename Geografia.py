67890ed99e3cc28483f2ba05cf3d97b4d64b4f24
Claudia Goncalves <claudia.goncalves@correo.unimet.edu.ve>
2026-08-05 22:45:36 -0400

    Metodo es_valido para detectar coordenadas nulas

--- Geografia.py
+++ Geografia.py
@@ -5,3 +5,11 @@ class Coordenada:
     def __init__(self, latitud, longitud):
         self.lat = latitud
         self.lon = longitud 
+    
+    def es_valido(self):
+        """
+        Retorna True si el punto tiene coordenadas reales, o False si son nulas.
+        """
+        if self.lat != None and self.lon != None:
+            return True 
+        return False  
