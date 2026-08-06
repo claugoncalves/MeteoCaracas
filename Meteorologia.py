18431b129d7f73b62b77b9c86415432afe067ed4
Claudia Goncalves <claudia.goncalves@correo.unimet.edu.ve>
2026-08-06 11:42:19 -0400

    Creacion de la clase ReporteEnVivo que representa el clima en tiempo real y el estado del cielo

--- Meteorologia.py
+++ Meteorologia.py
@@ -7,3 +7,10 @@ class RegistroBase:
         self.porcentaje_humedad = hum_porcentaje
         self.viento_kmh = vel_viento 
         
+class ReporteEnVivo(RegistroBase):
+    """
+    Clase hija que representa el clima en tiempo real, agregando el estado del cielo.
+    """
+    def __init__(self, temp_celsius, hum_porcentaje, vel_viento, id_cielo):
+        super().__init__(temp_celsius, hum_porcentaje, vel_viento)
+        self.id_cielo = id_cielo
