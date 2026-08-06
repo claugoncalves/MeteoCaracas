dc6b6f367742984cf45cb94ed813b4b6f0f87ac7
Claudia Goncalves <claudia.goncalves@correo.unimet.edu.ve>
2026-08-06 11:04:17 -0400

    Correccion de espacio al inicio de la clase SectorCaracas

--- Geografia.py
+++ Geografia.py
@@ -13,19 +13,18 @@ class Coordenada:
         if self.lat != None and self.lon != None:
             return True 
         return False  
-    
-    class SectorCaracas(Coordenada):
-        """
-        Hereda de Coordenada. Representa una zona especifica dentro de la capital.
-        """
-        def __init__(self, nom_distrito, nom_zona, latitud, longitud):
-            super().__init__(latitud, longitud)
-            self.distrito = nom_distrito
-            self.zona = nom_zona
-        
-        def imprimir_resumen(self):
-            """
-            Imprime la informacion basica del sector.
-            """
-            print("- " + self.distrito.upper() + ": " + self.zona + " (Coordenadas: " + str(self.lat) + "," + str(self.lon) + ")") 
+
+class SectorCaracas(Coordenada):
+    """
+    Hereda de Coordenada. Representa una zona especifica dentro de la capital.
+    """
+    def __init__(self, nom_distrito, nom_zona, latitud, longitud):
+        super().__init__(latitud, longitud)
+        self.distrito = nom_distrito
+        self.zona = nom_zona
             
+    def imprimir_resumen(self):
+        """
+        Imprime la informacion basica del sector.
+        """
+        print("- " + self.distrito.upper() + ": " + self.zona + " (Coordenadas: " + str(self.lat) + "," + str(self.lon) + ")") 
