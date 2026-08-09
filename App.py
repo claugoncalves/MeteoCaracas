5bc12732246c1304516b54108dffbc918ee3b3b3
Fabiola Gimenez <fgimenez@correo.unimet.edu.ve>
2026-08-09 17:48:57 -0400

    menu principal y consulta

--- App.py
+++ App.py
@@ -74,6 +74,85 @@ class Aplicacion:
     print('\n')
 
         
+    def iniciar(self):
+        """
+        Bucle de vida util del programa principal
+        """
+        if len(self.bd_sectores)==0:
+           print('Aplicacion detenida por falta de datos base')
+           return
+        
+        apagar = False
+        while apagar==False:
+            print('\n MENU METEOCARACAS')
+            print('1) Navegar por municipio')
+            print('2) Buscar sector por nombre')
+            print('3) Abrir modulo de estadisticas')
+            print('4) Generar reporte historico y graficas')
+            print('5) Salir del sistema')
+
+            eleccion=input('Ingresa una opcion (1-5):  ')
+
+            if eleccion=='1' or eleccion=='2':
+                objetivo=None
+                if eleccion=='1':
+                   objetivo= self.herramienta_filtro.explorar_por_distrito(self.bd_sectores)
+                elif objetivo=='2':
+                 objetivo= self.herramienta_filtro.buscar_palabra(self.bd_sectores)
+
+                if objetivo!=None:
+                  print('\n CLIMA EN VIVO:')
+                  objetivo.imprimir_resumen()
+
+                  respuesta_api=self.cliente_api.obtener_clima_vivo(objetivo.lat,objetivo.lon)
+
+                  if respuesta_api != None:
+                     respuesta_api.mostrar_panel()
+                     self.motor_datos.registrar_busqueda(objetivo, respuesta_api)
+
+            elif eleccion=='3':
+               self.motor_datos.panel_sesion(self.bd_sectores)    
+
+            elif eleccion=='4':
+                objetivo_hist=self.herramienta_filtro.buscar_palabra(self.bd_sectores)
+
+                if objetivo_hist != None:
+                    print("\n FORMATO REQUERIDO: AAAA-MM-DD ")
+                    f_ini=input('Fecha inicial: ')
+                    f_fin=input('Fecha final: ')
+
+                    print('Solicitando datos a Open-Meteo...')
+                    arr_diario=self.cliente_api.obtener_clima_pasado(objetivo_hist.lat, objetivo_hist.lon, f_ini, f_fin )
+
+                    if len(arr_diario)>0:
+                      df_generado=  self.motor_datos.procesar_historico(arr_diario, objetivo_hist.zona)
+                      menu_graficos=False
+                      while menu_graficos==False:
+                        print('\n1)Abrir graficos')
+                        print('2)Continuar')
+
+                        op_graf=input('Seleccione una opcion (1-2):  ')
+
+                        if op_graf=='1':
+                          print('Abriendo graficos...')
+                          self.motor_datos.generar_graficas(df_generado,objetivo_hist.zona)
+                          menu_graficos=True
+
+                        elif op_graf=='2':
+                           print('Omitiendo graficos...')
+                           menu_graficos=True
+                        else:
+                         print('Error: opcion no valida.Intente de nuevo')    
+
+            elif eleccion=='5':
+               print('Apagando...\n')
+               apagar=True
+            else:
+               print('Opcion desconocida')   
+
+
+
+
 
       
                   
